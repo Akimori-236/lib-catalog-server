7f437218fde12d048ec6923d704a235da3695cae
Wee Seng <wsng.try@gmail.com>
2024-04-18 19:08:25 +0800

    expressjs with mock data

--- server.js
+++ server.js
@@ -1,20 +1,87 @@
-const http = require("http");
-const hostname = "localhost";
-const port = 3000;
+const express = require("express");
+const bodyParser = require("body-parser");
+const host = "localhost";
+const port = 8080;
 
-const server = http.createServer((req, res) => {
-    const method = req.method;
-    const url = req.url;
+const app = express();
 
-    console.log(`${method} ${url}`)
+var dummyData = [
+    {
+        userID: 1,
+        name: "John",
+        email: "john@gmail.com",
+        age: "20",
+        password: "abc123",
+    },
+];
 
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/html");
-    res.end("<html><body>Welcome to Node JS!</body></html>");
+//middlewares and endpoints...
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+app.use(urlencodedParser); //attach body-parser middleware
+app.use(bodyParser.json()); //parse json data
+
+// GET ALL users
+app.get("/", (req, resp) => {
+    resp.status(200);
+    resp.type("json");
+    resp.send(dummyData);
 });
 
-server.listen(port, hostname, () => {
-    console.log(
-        `Server started and accessible via http://${hostname}:${port}/`
-    );
+// GET user by ID
+app.get("/user/:id", (req, resp) => {
+    //retrieve request param
+    var id = req.params.id;
+    console.info(`Searching for user #${id}`);
+
+    var found = false;
+    var foundUser = "";
+    for (var i = 0; i < dummyData.length; i++) {
+        var jsonUser = dummyData[i];
+        if (jsonUser.userID == id) {
+            found = true;
+            foundUser = jsonUser;
+            break;
+        }
+    }
+
+    //return the response
+    if (found) {
+        resp.status(200);
+        resp.type("json");
+        resp.send(foundUser);
+    } else {
+        resp.status(404);
+        resp.type("json");
+        resp.send(`{"Message":"User not found"}`);
+    }
+});
+
+// POST new user
+app.post("/user", (req, resp) => {
+        //retrieve req data
+        var name = req.body.name;
+        var email = req.body.email;
+        var age = req.body.age;
+        var password = req.body.password;
+
+        var maxID = dummyData[dummyData.length - 1].userID;
+        var newID = maxID + 1;
+        // construct user
+        var userJSON = {
+            userID: newID,
+            name: name,
+            email: email,
+            age: age,
+            password: password,
+        };
+        // add user to list
+        dummyData.push(userJSON);
+
+        //generate response
+        resp.status(201);
+        resp.send("Success");
+});
+
+app.listen(port, host, () => {
+    console.log(`Server hosted at http://${host}:${port} `);
 });

5d4e5b6565eb08ddbe433901c09d84b495573ddd
Wee Seng <wsng.try@gmail.com>
2024-04-20 00:58:17 +0800

    add cors

---
+++ server.ts
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dummyData = require("./json/YMHPage1.json");
+
+const host = "localhost";
+const port = 3000;
+const app = express();
+const corsOptions = {
+    origin: "http://localhost:8080",
+    optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+//middlewares and endpoints...
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+app.use(urlencodedParser); //attach body-parser middleware
+app.use(bodyParser.json()); //parse json data
+
+// GET ALL data
+app.get("/", (req, resp) => {
+    resp.status(200);
+    resp.type("json");
+    resp.send(dummyData);
+});
+
+app.listen(port, host, () => {
+    console.log(`Server hosted at http://${host}:${port} `);
+});
